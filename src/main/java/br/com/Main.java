d7c335f3ba330f2eb021c2ebb7597ce443125c60
Augusto <augusto@inovacommerce.com.br>
2024-02-02 17:18:23 -0300

    Desafio

--- src/main/java/br/com/Main.java
+++ src/main/java/br/com/Main.java
@@ -20,7 +20,7 @@ public class Main {
                 Ano de lançamento
                 """+ anoDeLancamento;
         System.out.println(sinopse);
-        
+                    
         int classificacao = (int) (media /2);
         System.out.println(classificacao);
         
